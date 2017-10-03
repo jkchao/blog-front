805bde9b9106da065c77f96f9e88a9d1d7a53c9f
三毛 <jkchaom@gmail.com>
2017-10-03 10:33:09 +0800

    Update heros.js

--- store/heros.js
+++ store/heros.js
@@ -9,17 +9,35 @@ export const state = () => ({
 })
 
 export const mutations = {
+  // 获取列表
   FETCH_HERO (state) {
     state.fetch = true
   },
-
+  
+  // 成功
   SET_HERO_SUCCESS (state, data) {
     state.data = data
     state.fetch = false
   },
-
+  
+  // 失败
   SET_HERO_FILE (state, data) {
     state.data = { list: [], pagination: {} }
     state.fetch = false
-  }
+  },
+
+  // 发布留言板
+  POST_ITEM (state) {
+    state.posting = true
+  },
+
+  POST_ITEM_SUCCESS (state, action) {
+    state.posting = false
+    state.data.pagination.total += 1
+    state.data.list.push(action.result)
+  },
+
+  POST_ITEM_FAILURE (state) {
+    state.posting = false
+  },
 }
