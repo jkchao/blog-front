90bb22bbd2909eb2e5d1b19f7bc262dba5e94d02
jkchao <jkchaom@gmail.com>
2017-12-02 22:19:45 +0800

    baidu push

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,7 @@ module.exports = {
     { src: '~/plugins/gravatar.js' },
     { src: '~plugins/clickOutside.js', ssr: false },
     { src: '~/plugins/ga.js', ssr: false },
+    { src: '~plugins/baidu-seo-push.js', ssr: false },
     { src: '~plugins/filter.js' },
     { src: '~plugins/finally.js' }
   ],
