13e9f4474458f9d461e8d97be2f7cde9156246f1
jkchao <jkchaom@gmail.com>
2018-12-02 19:23:33 +0800

    fix: images url

--- plugins/marked.js
+++ plugins/marked.js
@@ -29,8 +29,9 @@ const paragraphParse = text => {
 
 // 对图片进行弹窗处理, 及懒加载处理
 const imageParse = (src, title, alt) => {
-  const arr = src.split('/')
-  const link = 'https://static.jkchao.cn/' + arr[arr.length - 1]
+  const index = src.indexOf('/', 8)
+  const url = src.slice(index)
+  const link = 'https://static.jkchao.cn' + url
   return `<img
             src="${link}?imageMogr2/auto-orient/interlace/1/blur/1x0/quality/75|watermark/2/text/amtjaGFvLmNu/font/5qW35L2T/fontsize/720/fill/I0ZERkRGRA==/dissolve/100/gravity/SouthEast/dx/10/dy/10|imageslim"
             title="${title || alt || 'jkchao.cn'}"
