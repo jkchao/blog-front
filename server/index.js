238efd226c44f62aae6bc8744b51ebb3e953ae45
jkchao <jkchaom@gmail.com>
2019-02-11 17:25:26 +0800

    update env host

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ async function start() {
   const nuxt = new Nuxt(config)
   
   const {
-    host = process.env.HOST || '127.0.0.1',
+    host = process.env.HOST || '0.0.0.0',
     port = process.env.PORT || 3000
   } = nuxt.options.server
 
