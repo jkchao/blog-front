be6749420c86b34cccf1b6c15189c3419c413456
jkchao <jkchaom@gmail.com>
2019-02-13 14:38:14 +0800

    update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # service 还是使用 pm2
-pm2 stop service
+pm2 stop all
 
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
+# docker-compose -f docker-compose.yml build
+# docker-compose -f docker-compose.yml up -d
 
-pm2 restart service
+echo 'npm run build'
+
+npm run build
+
+echo 'npm run start'
+
+pm2 restart all
