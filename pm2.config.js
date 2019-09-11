513eba5f4009a1bf78166a2b404d5f93d333e875
jkchao <jkchaom@gmail.com>
2019-09-11 20:30:37 +0800

    fix: change pm2 log

--- pm2.config.js
+++ pm2.config.js
@@ -5,8 +5,8 @@ module.exports = {
       exec_mode: 'cluster',
       script: './server/index.js',
       log_date_format: 'YYYY-MM-DD HH:mm',
-      error_file: '/home/logs/nuxt',
-      out_file: '/home/logs/nuxt',
+      error_file: '/home/logs/nuxt/error.log',
+      out_file: '/home/logs/nuxt/out.log',
       env: {
         NODE_ENV: 'production'
       }
