260878bfbd6c3c17421b6e8850fb46f62932b1b7
jkchao <jkchaom@gmail.com>
2019-12-19 21:12:26 +0800

    feat: remove view

--- components/common/article.vue
+++ components/common/article.vue
@@ -26,7 +26,7 @@
               }}
             </span>
             <span class="hr"></span>
-            <span class="read"> {{ item.meta.views }} 次阅读</span>
+            <!-- <span class="read"> {{ item.meta.views }} 次阅读</span> -->
             <span class="hr"></span>
             <span class="comments"> {{ item.meta.comments }} 条评论</span>
             <span class="hr"></span>
